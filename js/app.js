985f8a9c5b0360afc77768317e3d6efe4950803c
bhoobeshm7-creator <bhoobeshm7@gmail.com>
2026-01-02 14:52:51 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,1 @@
-function magic() {
-    const quotes = [
-        "🚀 You clicked magic!",
-        "🎉 JavaScript is awesome!",
-        "🌈 Keep learning & building!",
-        "⚙️ Frontend power unlocked!"
-    ];
-    document.getElementById("result").innerText =
-        quotes[Math.floor(Math.random() * quotes.length)];
-}
+
